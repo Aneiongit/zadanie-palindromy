791c5b7979b4291353cc59b250d8130428a92bff
Anna M <83anei@gmail.com>
2021-09-25 17:34:23 +0100

    Updated code

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def palindrome(word):
-    word = word.lower()
-    return word == word.lower()[::-1]
+def palindromes(text):
+    text = str(text.lower())
+    results = []
+    for i in range(len(text)):
+        for j in range(0, i):
+            chunk = text[j:i + 1]
+
+            if chunk == chunk[::-1]:
+                results.append(chunk)
+    if len(results) > 0:
+        pali = max(results, key=len)
+        a = text.find(pali)
+        b = text.find(pali) + len(pali) - 1
+        print(a, b)
+    else:
+        print(-1, -1)
 
 
-x = palindrome('Anna')
-print(x)
+palindromes("kajak")
